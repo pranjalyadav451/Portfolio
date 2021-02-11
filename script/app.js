b4db86d0274a8795133c800301f05927b5f9c657
Pranjal Yadav <2019uec1008@mnit.ac.in>
2021-02-11 16:52:13 +0530

    minor changes in animation for mobile

--- script/app.js
+++ script/app.js
@@ -4,7 +4,7 @@ tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro-anim", { y: "-100%", duration: 1 }, "-=1");
 
-tl.fromTo(".greeting-wrapper", { opacity: 0, y: "-50%" }, { opacity: 1, y: "0%", duration: 1.5, ease: "elastic.out" }, delay = 3.5)
+tl.fromTo(".greeting-wrapper", { opacity: 0, y: "-20%" }, { opacity: 1, y: "0%", duration: 1.5, ease: "elastic.out" }, delay = 3.5)
 
 
 
